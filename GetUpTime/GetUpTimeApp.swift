c084e58e66cdb3417224471c5de506d788beec6c
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2024-09-07 16:10:52 -0300

    Initial Commit

---
+++ GetUpTime/GetUpTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetUpTimeApp.swift
+//  GetUpTime
+//
+//  Created by Rodrigo Souza on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GetUpTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GetUpTimeTests/GetUpTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  GetUpTimeTests.swift
+//  GetUpTimeTests
+//
+//  Created by Rodrigo Souza on 07/09/24.
+//
+
+import Testing
+@testable import GetUpTime
+
+struct GetUpTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
